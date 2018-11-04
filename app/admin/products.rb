dc6a8df1bcc1d39b9a2db9875447e5d6ef483b4a
christianuminga <cuminga@academic.rrc.ca>
2018-11-03 20:11:31 -0500

    implemented carrierwave gem for file uploads

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Product do
-    
-permit_params :name, :description, :price, :stock, :category_id
+
+permit_params :name, :description, :price, :stock, :category_id, :image
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
 	validates :name, length: { minimum: 3 }
 	validates :description, length: {maximum: 500}
     validates :price, :stock, numericality: true
+
+    mount_uploader :image, ImageUploader
     
     belongs_to :category
 end
