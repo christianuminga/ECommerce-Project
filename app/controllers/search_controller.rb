3456530bfb669627cae50e9395e24fb29762d1e5
Christian Uminga <christian.uminga@aceproject.space>
2018-11-15 17:25:38 -0600

    searching now filters description

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,7 @@ class SearchController < ApplicationController
   def results
     if params[:category] == "" && params[:q] != nil && params[:orderBy] == "1"
 
-      @products = Product.where('name LIKE ?', "%#{params[:q]}%").order("name ASC").page(params[:page]).per(5)
+      @products = Product.where('name LIKE ? OR description LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order("name ASC").page(params[:page]).per(5)
 
     elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "1"
 
@@ -17,11 +17,11 @@ class SearchController < ApplicationController
 
     elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "1"
 
-      @products = Product.where('name LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "#{params[:category]}").order("name ASC").page(params[:page]).per(5)
+      @products = Product.where('name LIKE ? OR description LIKE ? AND category_id LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("name ASC").page(params[:page]).per(5)
 
     elsif params[:category] == "" && params[:q] != nil && params[:orderBy] == "2"
 
-      @products = Product.where('name LIKE ?', "%#{params[:q]}%").order("created_at DESC").page(params[:page]).per(5)
+      @products = Product.where('name LIKE ? OR description LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order("created_at DESC").page(params[:page]).per(5)
 
     elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "2"
 
@@ -31,11 +31,11 @@ class SearchController < ApplicationController
 
     elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "2"
 
-      @products = Product.where('name LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "#{params[:category]}").order("created_at DESC").page(params[:page]).per(5) 
+      @products = Product.where('name LIKE ? OR description LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("created_at DESC").page(params[:page]).per(5)
 
     elsif params[:category] == "" && params[:q] != nil && params[:orderBy] == "3"
 
-      @products = Product.where('name LIKE ?', "%#{params[:q]}%").order("updated_at DESC").page(params[:page]).per(5)
+      @products = Product.where('name LIKE ? OR description LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order("updated_at DESC").page(params[:page]).per(5)
 
     elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "3"
 
@@ -45,7 +45,7 @@ class SearchController < ApplicationController
 
     elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "3"
 
-      @products = Product.where('name LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "#{params[:category]}").order("updated_at DESC").page(params[:page]).per(5) 
+      @products = Product.where('name LIKE ? OR description LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("updated_at DESC").page(params[:page]).per(5)
 
     end
   end
