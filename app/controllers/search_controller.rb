c6d63419e6ff2877f9efa5839415f5880a4aa06d
Christian Uminga <cuminga@academic.rrc.ca>
2018-11-14 20:59:21 -0600

    search refinery with order by

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,19 +3,47 @@ class SearchController < ApplicationController
   end
 
   def results
-    if params[:category] == "" && params[:q] != nil
+    if params[:category] == "" && params[:q] != nil && params[:orderBy] == "1"
 
-      @products = Product.where('name LIKE ?', "%#{params[:q]}%").page(params[:page])
+      @products = Product.where('name LIKE ?', "%#{params[:q]}%").order("name ASC").page(params[:page]).per(5)
 
-    elsif params[:category] != nil && params[:q] == ""
+    elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "1"
 
-      @products = Product.where('category_id LIKE ?', "#{params[:category]}").page(params[:page])
+      @products = Product.where('category_id LIKE ?', "#{params[:category]}").order("name ASC").page(params[:page]).per(5)
 
       @param = params[:category]
 
-    elsif params[:category] != nil && params[:q] != nil
+    elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "1"
 
-      @products = Product.where('name LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "#{params[:category]}").page(params[:page])
+      @products = Product.where('name LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "#{params[:category]}").order("name ASC").page(params[:page]).per(5)
+
+    elsif params[:category] == "" && params[:q] != nil && params[:orderBy] == "2"
+
+      @products = Product.where('name LIKE ?', "%#{params[:q]}%").order("created_at DESC").page(params[:page]).per(5)
+
+    elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "2"
+
+      @products = Product.where('category_id LIKE ?', "#{params[:category]}").order("created_at DESC").page(params[:page]).per(5)
+
+      @param = params[:category]
+
+    elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "2"
+
+      @products = Product.where('name LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "#{params[:category]}").order("created_at DESC").page(params[:page]).per(5) 
+
+    elsif params[:category] == "" && params[:q] != nil && params[:orderBy] == "3"
+
+      @products = Product.where('name LIKE ?', "%#{params[:q]}%").order("updated_at DESC").page(params[:page]).per(5)
+
+    elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "3"
+
+      @products = Product.where('category_id LIKE ?', "#{params[:category]}").order("updated_at DESC").page(params[:page]).per(5)
+
+      @param = params[:category]
+
+    elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "3"
+
+      @products = Product.where('name LIKE ? AND category_id LIKE?', "%#{params[:q]}%", "#{params[:category]}").order("updated_at DESC").page(params[:page]).per(5) 
 
     end
   end
