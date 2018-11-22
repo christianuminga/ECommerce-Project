5f1fc1adab0ed067cb5571ec490a05ca22c46600
christianuminga <cuminga@academic.rrc.ca>
2018-11-22 10:40:17 -0600

    modified gem file and database.yml for postgres

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ ruby '2.5.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -49,6 +49,12 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'web-console', '>= 3.3.0'
+  gem 'sqlite3'
+
+end
+
+group :production do
+  gem 'pg'
 end
 
 group :test do
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ApplicationRecord
 
     validates :name, :description, :price, :stock, presence: true
-	validates :name, length: { minimum: 3 }
-	validates :description, length: {maximum: 500}
+    validates :name, length: { minimum: 3 }
+    validates :description, length: {maximum: 500}
     validates :price, :stock, numericality: true
 
     mount_uploader :image, ImageUploader
     before_destroy :not_referenced_by_any_line_item
-    
+
     belongs_to :category
     has_many :line_items
 
