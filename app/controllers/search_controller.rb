75ea4580bea046bd9c304b6e6d597af06b0061fb
christianuminga <cuminga@academic.rrc.ca>
2018-11-22 11:30:09 -0600

    fixing sql for postgres

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,11 +11,11 @@ class SearchController < ApplicationController
 
     elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "1"
 
-      @products = Product.where('category_id LIKE ?', "#{params[:category]}").order("name ASC").page(params[:page]).per(5)
+      @products = Product.where('cast(category_id as text) LIKE ?', "#{params[:category]}").order("name ASC").page(params[:page]).per(5)
 
     elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "1"
 
-      @products = Product.where('(name LIKE ? OR description LIKE ?) AND category_id LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("name ASC").page(params[:page]).per(5)
+      @products = Product.where('(name LIKE ? OR description LIKE ?) AND cast(category_id as text) LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("name ASC").page(params[:page]).per(5)
 
     elsif params[:category] == "" && params[:q] != nil && params[:orderBy] == "2"
 
@@ -23,11 +23,11 @@ class SearchController < ApplicationController
 
     elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "2"
 
-      @products = Product.where('category_id LIKE ?', "#{params[:category]}").order("created_at DESC").page(params[:page]).per(5)
+      @products = Product.where('cast(category_id as text) LIKE ?', "#{params[:category]}").order("created_at DESC").page(params[:page]).per(5)
 
     elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "2"
 
-      @products = Product.where('(name LIKE ? OR description LIKE ?) AND category_id LIKE?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("created_at DESC").page(params[:page]).per(5)
+      @products = Product.where('(name LIKE ? OR description LIKE ?) AND cast(category_id as text) LIKE?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("created_at DESC").page(params[:page]).per(5)
 
     elsif params[:category] == "" && params[:q] != nil && params[:orderBy] == "3"
 
@@ -35,11 +35,11 @@ class SearchController < ApplicationController
 
     elsif params[:category] != nil && params[:q] == "" && params[:orderBy] == "3"
 
-      @products = Product.where('category_id LIKE ?', "#{params[:category]}").order("updated_at DESC").page(params[:page]).per(5)
+      @products = Product.where('cast(category_id as text) LIKE ?', "#{params[:category]}").order("updated_at DESC").page(params[:page]).per(5)
 
     elsif params[:category] != nil && params[:q] != nil && params[:orderBy] == "3"
 
-      @products = Product.where('(name LIKE ? OR description LIKE ?) AND category_id LIKE?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("updated_at DESC").page(params[:page]).per(5)
+      @products = Product.where('(name LIKE ? OR description LIKE ?) AND cast(category_id as text) LIKE?', "%#{params[:q]}%", "%#{params[:q]}%", "#{params[:category]}").order("updated_at DESC").page(params[:page]).per(5)
 
     end
   end
